fdbdc7097d8a0aea5b3b6abc5f4efc423f0929ae
Michael Kopp <kopp.michael@yahoo.de>
2020-11-21 22:52:22 +0100

    place ships randomly

--- game.py
+++ game.py
@@ -11,11 +11,13 @@ If Python and Arcade are installed, this example can be run from the command lin
 python -m arcade.examples.array_backed_grid_sprites_1
 """
 import arcade
-from typing import List, Optional
+from random import choice, randint
+from typing import List, Optional, Tuple
+
 
 # Set how many rows and columns we will have
-ROW_COUNT = 15
-COLUMN_COUNT = 15
+ROW_COUNT = 12
+COLUMN_COUNT = 12
 
 # This sets the WIDTH and HEIGHT of each grid location
 WIDTH = 30
@@ -63,6 +65,14 @@ class Ship:
         return set(self.hits) == set(self.occupied_space)
         
 
+def _get_all_cells_in_and_around_ship(ship: Ship) -> List[Tuple[int, int]]:
+    cells = set()
+    for row, col in ship.occupied_space:
+        for row_delta in [+1, 0, -1]:
+            for col_delta in [+1, 0, -1]:
+                cells.add((row + row_delta, col + col_delta))
+    return list(cells)
+
 
 class MyGame(arcade.Window):
     """
@@ -75,6 +85,13 @@ class MyGame(arcade.Window):
         """
         super().__init__(width, height, title)
 
+        self.number_of_shots = 0
+        self.ships_to_sink_of_size = {
+            3: 4,
+            4: 3,
+            5: 3,
+            6: 2,
+        }
         self.ships: List[Ship] = []
         self._place_ships()
 
@@ -102,10 +119,23 @@ class MyGame(arcade.Window):
                 sprite.center_y = y
                 self.grid_sprite_list.append(sprite)
 
-
     def _place_ships(self):
-        ship = Ship(4, 4, 4, "column")
-        self.ships.append(ship)
+        for ship_size in sorted(self.ships_to_sink_of_size.keys(), reverse=True):
+            ships_to_place = self.ships_to_sink_of_size[ship_size]
+            ships_placed = 0
+            while ships_placed < ships_to_place:
+                orientation = choice(["row", "column"])
+                row = randint(0, ROW_COUNT - ship_size)
+                col = randint(0, COLUMN_COUNT - ship_size)
+                possible_ship = Ship(ship_size, row, col, orientation)
+                ship_is_in_collission = False
+                for row, col in _get_all_cells_in_and_around_ship(possible_ship):
+                    if self._get_ship_at(row, col) is not None:
+                        ship_is_in_collission = True
+                        break
+                if not ship_is_in_collission:
+                    ships_placed += 1
+                    self.ships.append(possible_ship)
 
     def _get_ship_at(self, row: int, column: int) -> Optional[Ship]:
         for ship in self.ships:
@@ -147,6 +177,54 @@ class MyGame(arcade.Window):
 
         self.grid_sprite_list.draw()
 
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.Q:
+            print("Bye")
+            raise SystemExit()
+        elif key == arcade.key.C:
+            print("Cheeeeeter")
+            for row in range(ROW_COUNT):
+                for col in range(COLUMN_COUNT):
+                    self._reveal_grid_cell_kind(row, col)
+            self.resync_grid_with_sprites()
+
+
+    def _reveal_grid_cell_kind(self, row: int, column: int):
+        ship = self._get_ship_at(row, column)
+        if ship is None:
+            self.grid[row][column] = "water"
+        else:
+            if ship.is_sunk():
+                for ship_row, ship_col in ship.occupied_space:
+                    self.grid[ship_row][ship_col] = "sunk ship"
+            else:
+                self.grid[row][column] = "ship"
+
+    def _check_game_is_won(self):
+        ships_to_sink = sum(self.ships_to_sink_of_size.values())
+        print(f"Still {ships_to_sink} ships to sink.")
+        if ships_to_sink == 0:
+            print(f"You Won after {self.number_of_shots} shots.")
+
+    def shoot_at(self, row, column):
+        self.number_of_shots += 1
+        ship = self._get_ship_at(row, column)
+        if ship is not None:
+            ship.hit_at(row, column)
+            if ship.is_sunk():
+                self.ships_to_sink_of_size[ship.length] -= 1
+                self._check_game_is_won()
+
+    def _status_text_ships_to_sink(self):
+        text = "To sink: "
+        for size, number_of_ships_to_find in self.ships_to_sink_of_size.items():
+            if number_of_ships_to_find == 0:
+                draw_character = '☒'
+            else:
+                draw_character = '☐'
+            text += f"{number_of_ships_to_find} x {size * draw_character}   "
+        return text
+
     def on_mouse_press(self, x, y, button, modifiers):
         """
         Called when the user presses a mouse button.
@@ -163,20 +241,13 @@ class MyGame(arcade.Window):
         if row < ROW_COUNT and column < COLUMN_COUNT:
 
             if self.grid[row][column] == "unknown":
-                hit_ship = self._get_ship_at(row, column)
-                if hit_ship is None:
-                    self.grid[row][column] = "water"
-                else:
-                    hit_ship.hit_at(row, column)
-                    if hit_ship.is_sunk():
-                        for ship_row, ship_col in hit_ship.occupied_space:
-                            self.grid[ship_row][ship_col] = "sunk ship"
-                    else:
-                        self.grid[row][column] = "ship"
+                self.shoot_at(row, column)
+                self._reveal_grid_cell_kind(row, column)
             else:
                 print(f"Grid Cell ({row}, {column}) was already known")
 
         self.resync_grid_with_sprites()
+        print(self._status_text_ships_to_sink())
 
 
 def main():
